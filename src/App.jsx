d4d910ec441d454f3024189f36fcfbb99b06fb5b
Phuc000 <truongvinhphuc2003@gmail.com>
2023-12-08 14:25:07 +0700

    feat: cartcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, Login, Cart, Category, BuyProduct, Store } from "./Pages";
+import { CartProvider } from './Context/CartContext';
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router>
-      <div className="App">
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Category/:categoryName" element={<Category />} />
-            <Route path="/Cart" element={<Cart />} />
-            <Route path="/buy-product/:productId" element={<BuyProduct />} />
-            <Route path="/store/:storeId" element={<Store />} />
-          </Routes>
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/Category/:categoryName" element={<Category />} />
+              <Route path="/Cart" element={<Cart />} />
+              <Route path="/buy-product/:productId" element={<BuyProduct />} />
+              <Route path="/store/:storeId" element={<Store />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </CartProvider>
   );
 }
 
