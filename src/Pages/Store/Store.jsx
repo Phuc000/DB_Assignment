05e35379c971854c67c9e88f3044b63c73aac8f9
Phuc000 <truongvinhphuc2003@gmail.com>
2023-12-10 14:20:52 +0700

    fix: store name

--- src/Pages/Store/Store.jsx
+++ src/Pages/Store/Store.jsx
@@ -8,6 +8,7 @@ import "./Store.css";
 const Store = () => {
   const [products, setProducts] = useState([]);
     const { storeId } = useParams();
+    const [ store, setStore ] = useState();
 
     useEffect(() => {
       // Fetch category-specific data from JSON file based on categoryName
@@ -20,53 +21,34 @@ const Store = () => {
           console.log('Fetched Data:', response.data);
           return response.data;
         })
-        .then(async (data) => {
+        .then((data) => {
           console.log('Fetched Data:', data);
-          const promoProductsData = [];
+          setProducts(data);
+        })
+        .catch((error) => console.error(`Error fetching store ${storeId} data:`, error));
 
-          for (const product of data) {
-            const promotionInfo = await fetchPromotionInfo(product.ProductID);
-            promoProductsData.push({
-              ...product,
-              promotions: promotionInfo,
-              totalDiscount: calculateTotalDiscount(promotionInfo),
-            });
-          }
-
-          setProducts(promoProductsData);
+      axios.get(`http://localhost:8080/store/${storeId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          console.log('Fetched Data:', response.data);
+          return response.data;
+        })
+        .then((data) => {
+          console.log('Fetched Data:', data);
+          setStore(data);
         })
         .catch((error) => console.error(`Error fetching store ${storeId} data:`, error));
     }, [storeId]);
-
-    const fetchPromotionInfo = async (productId) => {
-      try {
-        const response = await axios.get(`http://localhost:8080/products/promotionfromproduct/${productId}`, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-        return response.data;
-      } catch (error) {
-        console.error(`Error fetching promotion info for product ${productId}:`, error);
-        return [];
-      }
-    };
-  
-    const calculateTotalDiscount = (promotions) => {
-      if (!promotions || promotions.length === 0) {
-        return 0; // No discounts
-      }
-      const totalDiscount = promotions.reduce((total, promotion) => total + promotion.Discount, 0);
-      // Ensure the total discount does not exceed 0.99
-      return Math.min(totalDiscount, 0.99);
-    };
 
   return (
     <div className="store">
       <Header/>
       <div className="store-content">
       <header className="products__header container">
-          <h2 className="subtitle subtitle--products">Store: {storeId}</h2>
+        {store?.Name && <h2 className="subtitle subtitle--products">{store.Name}</h2>}
         </header>
         <div className="products__container container">
           {products.map((product) => (
