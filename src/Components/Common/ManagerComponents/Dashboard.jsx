076d957e87c9ad633e6f466580823d941e9d4e47
Phuc000 <truongvinhphuc2003@gmail.com>
2024-10-30 11:09:46 +0700

    create promotion UI

--- src/Components/Common/ManagerComponents/Dashboard.jsx
+++ src/Components/Common/ManagerComponents/Dashboard.jsx
@@ -1,11 +1,55 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import {ShowProduct } from "../../../Components";
 import axios from "axios";
+import "./Dashboard.scss";
 
 const Dashboard = () => {
+    const [products, setProducts] = useState([]);
+    const storeId = 1;
+    const [ store, setStore ] = useState();
+
+    useEffect(() => {
+      // Fetch category-specific data from JSON file based on categoryName
+      axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/products/store/${storeId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          console.log('Fetched Data:', response.data);
+          return response.data;
+        })
+        .then((data) => {
+          console.log('Fetched Data:', data);
+          setProducts(data);
+        })
+        .catch((error) => console.error(`Error fetching store ${storeId} data:`, error));
+
+      axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/store/${storeId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          console.log('Fetched Data:', response.data);
+          return response.data;
+        })
+        .then((data) => {
+          console.log('Fetched Data:', data);
+          setStore(data);
+        })
+        .catch((error) => console.error(`Error fetching store ${storeId} data:`, error));
+    }, [storeId]);
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <div className="products__container_2">
+                {products.map((product) => (
+                    <ShowProduct key={product.ProductID} product={product} storeId={storeId} />
+                ))}
+            </div>
         </div>
     );
 
