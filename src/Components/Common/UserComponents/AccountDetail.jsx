e0c6134a060f671eec20b02cee48660cbbb48eed
Phuc000 <truongvinhphuc2003@gmail.com>
2024-11-05 12:04:28 +0700

    adjust color addtocartbutton

--- src/Components/Common/UserComponents/AccountDetail.jsx
+++ src/Components/Common/UserComponents/AccountDetail.jsx
@@ -4,12 +4,30 @@ import axios from "axios";
 import "./AccountDetail.scss";
 
 const AccountDetails = () => {
+    const [buttonClass, setButtonClass] = useState('');
+
+    const saveUserDetails = async () => {
+        console.log('Saving user details...');
+        setButtonClass('onclic');
+        setTimeout(() => {
+          setButtonClass('validate');
+          setTimeout(() => {
+            setButtonClass('');
+          }, 1250);
+        }, 2250);
+    }
+
 
     return (
         <div className="account_details_wrapper">
             <InfoForm title="Account Details" />
             {/* save button  */}
-            <button className="save_button">Save</button>
+            <button
+              id='save-info-button'
+              className={`save-info ${buttonClass}`}
+              onClick={saveUserDetails}
+            >
+            </button>
         </div>
     );
 
